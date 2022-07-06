a03f0e4ec2e826a252d1fad6a50ee585d98293f0
Ali Tariq <alibabapk@aol.com>
2022-07-06 16:22:30 +0500

    transition effect added

--- js/my.js
+++ js/my.js
@@ -1,16 +1,18 @@
 
-function showData(){
-const form = document.getElementById('form');
-const formData = new FormData(form);
-
-const output = document.getElementById('output');
-
-for (const [key, value] of formData) {
-  output.textContent += `${key}: ${value}\n`;
-}
-
-}
-
-function showalert(){
-    alert("who called me?");
+window.addEventListener('scroll',reveal);
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+  // console.log(document.querySelectorAll('.reveal'));
+  for (let index = 0; index < reveals.length; index++) {
+    var windowheight = window.innerHeight;
+  var revealtop = reveals[index].getBoundingClientRect().top;
+  var revealpoint = 150;
+  if (revealtop < windowheight - revealpoint){
+    reveals[index].classList.add('active');
+  }
+  else{
+    reveals[index].classList.remove('active');
+  }
+  }
+ 
 }
